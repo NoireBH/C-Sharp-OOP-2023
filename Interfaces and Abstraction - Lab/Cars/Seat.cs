20483df887dfb06669b3b06c59e8a8ca675077f1
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-01-30 18:22:33 +0200

    Added Interfaces And Abstraction Lab  projects

---
+++ Interfaces and Abstraction - Lab/Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
