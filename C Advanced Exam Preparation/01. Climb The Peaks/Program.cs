5813b6c2c1a685fe848b5512dcbf5f975879a077
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-02-07 12:45:59 +0200

    Added c-Sharp advanced Exam projects

---
+++ C Advanced Exam Preparation/01. Climb The Peaks/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Climb_The_Peaks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var conqueredPeaks = new Queue<string>();
+
+            var foodSupplies = new Stack<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+            var stamina = new Queue<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+
+            var peaks = new Dictionary<string, int>()
+            {
+                { "Vihren", 80},
+                { "Kutelo", 90},
+                { "Banski Suhodol", 100},
+                { "Polezhan", 60},
+                { "Kamenitza", 70}
+            };
+
+            var peakNames = new Queue<string>();
+            foreach (var peak in peaks)
+            {
+                peakNames.Enqueue(peak.Key);
+            }
+
+            int currentDay = 0;
+
+            while (currentDay <= 7 && stamina.Count > 0 && peakNames.Count > 0)
+            {
+                int currentResourceSum = foodSupplies.Pop() + stamina.Dequeue();
+                int currentPeakValue = peaks[peakNames.Peek()];
+
+                if (currentResourceSum >= currentPeakValue)
+                {
+                    conqueredPeaks.Enqueue(peakNames.Dequeue());                 
+                }
+
+                currentDay++;
+            }
+
+            if (peakNames.Count == 0)
+            {
+                Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week" +
+                    " -> @FIVEinAWEEK");
+               
+            }
+
+            else if(peakNames.Count > 0 || currentDay == 7)
+            {
+                Console.WriteLine("Alex failed! He has to organize his journey better next time" +
+                    " -> @PIRINWINS");
+            }
+
+            if (conqueredPeaks.Count > 0)
+            {
+                Console.WriteLine("Conquered peaks:");
+                Console.WriteLine(String.Join(Environment.NewLine, conqueredPeaks));
+            }
+
+        }
+    }
+}
---
+++ C Advanced Exam Preparation/02. Navy Battle/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _02._Navy_Battle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] battlefield = new char[n, n];
+
+            int submarineRow = 0;
+            int submarineCol = 0;
+
+            int submarineHp = 3;
+            int battleCruisers = 3;
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowInfo = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    battlefield[row, col] = rowInfo[col];
+
+                    if (battlefield[row, col] == 'S')
+                    {   
+                        submarineRow = row;
+                        submarineCol = col;
+                        battlefield[row, col] = '-';
+                    }
+                }
+            }
+
+            while (battleCruisers > 0 && submarineHp > 0)
+            {
+                string direction = Console.ReadLine().ToLower();
+
+                if (direction == "up")
+                {
+                    submarineRow--;
+                }
+
+                else if (direction == "down")
+                {
+                    submarineRow++;
+                }
+
+                else if (direction == "left")
+                {
+                    submarineCol--;
+                }
+
+                else if (direction == "right")
+                {
+                    submarineCol++;
+                }
+
+                if (battlefield[submarineRow, submarineCol] == 'C')
+                {
+                    battlefield[submarineRow, submarineCol] = '-';
+                    battleCruisers--;
+                }
+
+                else if (battlefield[submarineRow, submarineCol] == '*')
+                {
+                    battlefield[submarineRow, submarineCol] = '-';
+                    submarineHp--;
+                }
+            }
+
+            battlefield[submarineRow, submarineCol] = 'S';
+
+            if (battleCruisers == 0)
+            {
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+            }
+
+            else if (submarineHp == 0)
+            {
+                Console.WriteLine($"Mission failed, U-9 disappeared!" +
+                    $" Last known coordinates [{submarineRow}, {submarineCol}]!");
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(battlefield[row,col]);
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
