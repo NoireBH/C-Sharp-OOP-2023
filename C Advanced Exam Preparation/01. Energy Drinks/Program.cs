daca5f85d402f2f21244230e76ddb3802a980920
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-02-11 12:36:25 +0200

    Added exam prep projects

---
+++ C Advanced Exam Preparation/01. Energy Drinks/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Energy_Drinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalCaffeine = 0;
+            const int maxCaffeine = 300;
+
+            var milligramsOfCaffeine = new Stack<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+            var energyDrinks = new Queue<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+
+            while (energyDrinks.Count > 0 && milligramsOfCaffeine.Count > 0)
+            {
+                int currentCaffeine = milligramsOfCaffeine.Peek() * energyDrinks.Peek();
+
+                if (currentCaffeine + totalCaffeine <= maxCaffeine)
+                {
+                    milligramsOfCaffeine.Pop();
+                    energyDrinks.Dequeue();
+                    totalCaffeine += currentCaffeine;
+                }
+
+                else
+                {
+                    milligramsOfCaffeine.Pop();
+                    int moveToLast = energyDrinks.Dequeue();
+                    energyDrinks.Enqueue(moveToLast);
+                    totalCaffeine -= 30;
+
+                    if (totalCaffeine < 0)
+                    {
+                        totalCaffeine = 0;
+                    }
+                }
+                    
+                
+            }
+
+            if (energyDrinks.Count > 0)
+            {
+                Console.WriteLine($"Drinks left: { string.Join(", ", energyDrinks)}");
+            }
+
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+
+            Console.WriteLine($"Stamat is going to sleep with {totalCaffeine} mg caffeine.");
+        }
+    }
+}
---
+++ C Advanced Exam Preparation/02. Rally Racing/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Rally_Racing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsAndCols = int.Parse(Console.ReadLine());
+            string[,] track = new string[rowsAndCols, rowsAndCols];
+            string racingNumber = Console.ReadLine();
+
+            int kilometersPassed = 0;
+            int RaceCarRow = 0;
+            int RaceCarCol = 0;
+
+            bool finishedRace = false;
+
+            for (int row = 0; row < track.GetLength(0); row++)
+            {
+                string[] rowInput = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < track.GetLength(1); col++)
+                {
+                    
+
+                    track[row, col] = rowInput[col];
+                }
+            }
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string direction = input.ToLower();
+                
+                if (direction == "up")
+                {
+                    RaceCarRow--;
+                }
+
+                else if (direction == "down")
+                {
+                    RaceCarRow++;
+                }
+
+                else if (direction == "left")
+                {
+                    RaceCarCol--;
+                }
+
+                else if (direction == "right")
+                {
+                    RaceCarCol++;
+                }
+
+                if (track[RaceCarRow,RaceCarCol] == "T")
+                {
+                    kilometersPassed += 30;
+                    track[RaceCarRow, RaceCarCol] = ".";
+                    bool tunnelPassed = false;
+
+                    for (int r = 0; r < rowsAndCols; r++)
+                    {
+                        for (int c = 0; c < rowsAndCols; c++)
+                        {
+                            if (track[r,c] == "T")
+                            {
+                                track[r, c] = ".";
+                                RaceCarRow = r;
+                                RaceCarCol = c;
+                                tunnelPassed = true;
+                                break;
+                            }
+                        }
+
+                        if (tunnelPassed)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                else if (track[RaceCarRow, RaceCarCol] == "F")
+                {
+                    kilometersPassed += 10;
+                    track[RaceCarRow, RaceCarCol] = ".";                    
+                    finishedRace = true;
+                    break;
+                }
+
+                else
+                {
+                    kilometersPassed += 10;
+                }
+            }
+
+            track[RaceCarRow, RaceCarCol] = "C";
+
+            if (!finishedRace)
+            {
+                Console.WriteLine($"Racing car {racingNumber} DNF.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Racing car {racingNumber} finished the stage!");
+            }
+
+            Console.WriteLine($"Distance covered {kilometersPassed} km.");
+
+            for (int row = 0; row < rowsAndCols; row++)
+            {
+                for (int col = 0; col < rowsAndCols; col++)
+                {
+                    Console.Write(track[row,col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
