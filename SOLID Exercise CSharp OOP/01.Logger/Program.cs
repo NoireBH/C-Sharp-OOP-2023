e3023814e94e7c7806d6cd0804fb5e312f8f949e
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-03-05 11:09:52 +0200

    put bisuness logic in method

--- SOLID Exercise CSharp OOP/01.Logger/Program.cs
+++ SOLID Exercise CSharp OOP/01.Logger/Program.cs
@@ -5,20 +5,24 @@ using _01.Logger.Models;
 using _01.Logger.Models.Interfaces;
 using _01.Logger.Models.Layouts;
 
-var simpleLayout = new SimpleLayout();
-var logfile = new LogFile();
-var consoleAppender = new ConsoleAppender(simpleLayout,logfile);
-var fileappender = new FileAppender(simpleLayout, logfile);
-consoleAppender.ReportLevel = ReportLevel.Error;
-fileappender.ReportLevel = ReportLevel.Error;
 
-var logger = new Logger(consoleAppender,fileappender);
+BisunessLogic();
 
-logger.Info("Everything seems fine");
-logger.Warning("Warning: ping is too high - disconnect imminent");
-logger.Error("Error parsing request");
-logger.Critical("No connection string found in App.config");
-logger.Fatal("mscorlib.dll does not respond");
+static void BisunessLogic()
+{
+    var simpleLayout = new SimpleLayout();
+    var logfile = new LogFile();
+    var consoleAppender = new ConsoleAppender(simpleLayout, logfile);
+    var fileappender = new FileAppender(simpleLayout, logfile);
+    consoleAppender.ReportLevel = ReportLevel.Error;
+    fileappender.ReportLevel = ReportLevel.Error;
 
+    var logger = new Logger(consoleAppender, fileappender);
 
+    logger.Info("Everything seems fine");
+    logger.Warning("Warning: ping is too high - disconnect imminent");
+    logger.Error("Error parsing request");
+    logger.Critical("No connection string found in App.config");
+    logger.Fatal("mscorlib.dll does not respond");
+}
 
