a9a8119c46fbf6e22f5179efdc9259cff3db8d78
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-03-09 17:29:22 +0200

    Added balanced parentheses project

--- Data Structures/03. Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Data Structures/03. Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,63 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            var openBrackets = new Stack<char>();
+            bool isBalanced = false;
+
+            foreach (char bracket in parentheses)
+            {
+                if (bracket == '{' || bracket == '[' || bracket == '(')
+                {
+                    openBrackets.Push(bracket);
+                }
+
+                else if (bracket == '}' || bracket == ']' || bracket == ')')
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    char lastOpen = openBrackets.Pop();
+
+                    if (lastOpen == '{' && bracket == '}')
+                    {
+                        isBalanced = true;
+                    }
+
+                    else if (lastOpen == '[' && bracket == ']')
+                    {
+                        isBalanced = true;
+                    }
+
+                    else if (lastOpen == '(' && bracket == ')')
+                    {
+                        isBalanced = true;
+                    }
+
+                    else
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+
+
+                }
+
+
+            }
+
+            if (isBalanced)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
         }
     }
-}
+    }
+
