d16d353be332429351ed962de2a85f46c9149706
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-03-26 18:30:34 +0300

    Added class structure of a project

---
+++ C-Sharp OOP exam prep/HotelBookingApp/Models/Bookings/Booking.cs
@@ -0,0 +1,91 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Bookings
+{
+    public class Booking : IBooking
+    {
+        private int residenceDuration;
+        private int adultsCount;
+        private int childrenCount;      
+
+        public Booking(IRoom room, int residenceDuration, int adultsCount, int childrenCount, int bookingNumber)
+        {
+            Room = room;
+            ResidenceDuration = residenceDuration;
+            AdultsCount = adultsCount;
+            ChildrenCount = childrenCount;
+            BookingNumber = bookingNumber;
+        }
+
+        public IRoom Room {get; private set;}
+        
+
+        public int ResidenceDuration
+        {
+            get { return residenceDuration; }
+
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.DurationZeroOrLess);
+                }
+
+                residenceDuration = value; 
+            }
+        }
+
+        public int AdultsCount
+        {
+            get { return adultsCount; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.AdultsZeroOrLess));
+                }
+
+                adultsCount = value;
+            }
+        }
+
+
+        public int ChildrenCount
+        {
+            get { return childrenCount; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.ChildrenNegative));
+                }
+
+                childrenCount = value;
+            }
+        }
+
+
+        public int BookingNumber { get; private set; }
+
+        public string BookingSummary()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Booking number: {BookingNumber}");
+            sb.AppendLine($"Room type: {Room.GetType().Name}");
+            sb.AppendLine($"Adults: {AdultsCount} Children: {ChildrenCount}");
+            sb.AppendLine($"Total amount paid: {TotalPaid():F2} $");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private double TotalPaid() => Math.Round(ResidenceDuration * Room.PricePerNight, 2);
+
+    }
+}
---
+++ C-Sharp OOP exam prep/HotelBookingApp/Models/Rooms/Room.cs
@@ -0,0 +1,42 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public abstract class Room : IRoom
+    {
+        private int bedCapacity;
+        private double pricePerNight = 0;
+        public int BedCapacity {get; private set;}
+
+        public Room(int bedCapactity)
+        {
+            bedCapacity = bedCapactity;
+            
+        }
+
+        public double PricePerNight
+        {
+            get { return pricePerNight; }
+
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.PricePerNightNegative));
+                }
+
+                pricePerNight = value; 
+            }
+        }
+
+
+        public void SetPrice(double price)
+        {
+            PricePerNight = price;
+        }
+    }
+}
