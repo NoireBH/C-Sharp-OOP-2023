74c0dfac6acb5809a7bcfa098232dae5959cb212
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-03-29 18:37:55 +0300

    Added class models

---
+++ C-Sharp OOP exam prep/PlanetWars/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,37 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        private double cost;
+        private int enduranceLevel = 1;
+
+        public MilitaryUnit(double cost)
+        {
+            Cost = cost;
+        }
+
+        public double Cost { get; private set; }
+
+        public int EnduranceLevel
+        {
+            get { return enduranceLevel; }
+           private set { enduranceLevel = value; }
+        }
+        public void IncreaseEndurance()
+        {            
+            EnduranceLevel++;
+
+            if (EnduranceLevel > 20)
+            {
+                EnduranceLevel = 20;
+                throw new ArgumentException(ExceptionMessages.EnduranceLevelExceeded);
+            }
+        }
+    }
+}
---
+++ C-Sharp OOP exam prep/PlanetWars/Models/Planets/Planet.cs
@@ -0,0 +1,112 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Planets
+{
+    public class Planet : IPlanet
+    {
+        private string name;
+        private double budget;
+        private double militaryPower;
+        private List<IMilitaryUnit> army;
+        private List<IWeapon> weapons;
+
+        public Planet(string name, double budget)
+        {
+            Name = name;
+            Budget = budget;
+            army = new List<IMilitaryUnit>();
+            weapons = new List<IWeapon>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPlanetName);
+                }
+
+                name = value;
+            }
+        }
+
+
+        public double Budget
+        {
+            get { return budget; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBudgetAmount);
+                }
+
+                budget = value;
+            }
+        }
+
+        public double MilitaryPower
+        {
+            get { return militaryPower; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBudgetAmount);
+                }
+
+                militaryPower = value;
+            }
+        }
+
+        private void CalculateMilitaryPower()
+        {
+
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Army => throw new NotImplementedException();
+
+        public IReadOnlyCollection<IWeapon> Weapons => throw new NotImplementedException();
+
+        public void AddUnit(IMilitaryUnit unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PlanetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Profit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Spend(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrainArmy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
