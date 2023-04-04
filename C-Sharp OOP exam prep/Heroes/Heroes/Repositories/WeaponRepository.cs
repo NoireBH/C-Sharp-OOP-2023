c9a9e1ac757474b25e71dbebbd590b56046212aa
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-04-04 17:10:49 +0300

    Added class models

---
+++ C-Sharp OOP exam prep/Heroes/Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,42 @@
+﻿using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => weapons;
+
+        public void Add(IWeapon model)
+        {
+            weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return weapons.FirstOrDefault(h => h.Name == name);
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            if (weapons.Any(h => h == model))
+            {
+                weapons.Remove(model);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
